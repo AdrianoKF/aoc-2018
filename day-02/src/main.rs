b9cc565fd1427109b5b6d9ea674187967d294188
Adrian Rumpold <a.rumpold@gmail.com>
2018-12-03 22:42:43 +0100

    Second part of day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -8,34 +8,56 @@ use common::aoc::util;
 
 #[cfg(test)]
 mod tests {
-    use Counts;
-    use count;
+    mod counts {
+        use count;
+        use Counts;
 
-    #[test]
-    fn count_none() {
-        let word = "abcdef";
-        assert_eq!(Counts { word: word.to_string(), doubles: 0, triples: 0 }, count(word));
+        #[test]
+        fn count_none() {
+            let word = "abcdef";
+            assert_eq!(Counts { word: word.to_string(), doubles: 0, triples: 0 }, count(word));
+        }
+
+        #[test]
+        fn count_double() {
+            let word = "abbcde";
+            assert_eq!(Counts { word: word.to_string(), doubles: 1, triples: 0 }, count(word));
+        }
+
+        #[test]
+        fn count_triple() {
+            let word = "aabacd";
+            assert_eq!(Counts { word: word.to_string(), doubles: 0, triples: 1 }, count(word));
+        }
+
+        #[test]
+        fn count_both() {
+            let word = "bababc";
+            assert_eq!(Counts { word: word.to_string(), doubles: 1, triples: 1 }, count(word));
+        }
     }
 
-    #[test]
-    fn count_double() {
-        let word = "abbcde";
-        assert_eq!(Counts { word: word.to_string(), doubles: 1, triples: 0 }, count(word));
-    }
+    mod distance {
+        use distance;
 
-    #[test]
-    fn count_triple() {
-        let word = "aabacd";
-        assert_eq!(Counts { word: word.to_string(), doubles: 0, triples: 1 }, count(word));
-    }
+        #[test]
+        fn distance_equal() {
+            assert_eq!(0, distance("abcdef", "abcdef"));
+        }
 
-    #[test]
-    fn count_both() {
-        let word = "bababc";
-        assert_eq!(Counts { word: word.to_string(), doubles: 1, triples: 1 }, count(word));
+        #[test]
+        fn distance_one() {
+            assert_eq!(1, distance("abcdef", "abxdef"));
+        }
+
+        #[test]
+        fn distance_two() {
+            assert_eq!(2, distance("abcdef", "abxyef"));
+        }
     }
 }
 
+/// Read the problem set line by line
 fn read_input() -> Result<Vec<String>> {
     let filename = util::get_filename()?;
     let mut f = File::open(filename)?;
@@ -51,22 +73,41 @@ struct Counts {
     triples: usize,
 }
 
-fn count(data: &str) -> Counts {
+/// Calculate number of doubles and triples (repeated characters) in `word`
+fn count(word: &str) -> Counts {
     let mut freq = [0usize; 26];
 
     // Count the frequency of each character
-    for ch in data.chars() {
+    for ch in word.chars() {
         let ascii = ch as u8;
         let idx = (ascii - 'a' as u8) as usize;
         freq[idx] += 1;
     }
     Counts {
-        word: data.to_string(),
+        word: word.to_string(),
         doubles: freq.iter().filter(|&&c| c == 2).count(),
         triples: freq.iter().filter(|&&c| c == 3).count(),
     }
 }
 
+/// Calculate number of different characters between `a` and `b`
+fn distance(a: &str, b: &str) -> usize {
+    assert_eq!(a.len(), b.len());
+    a.chars().zip(b.chars()).map(|(c1, c2)| if c1 == c2 { 0 } else { 1 }).sum()
+}
+
+/// Find a pair of words in `data` that only differs in a single position
+fn find_candidates(data: Vec<String>) -> Option<(String, String)> {
+    for word in data.clone() {
+        match data.clone().iter().find(|&w2| distance(word.as_str(), w2.as_str()) == 1) {
+            Some(other) => return Some((word, other.clone())),
+            _ => {}
+        }
+    }
+    None
+}
+
+/// Solution for part 1 -> checksum calculation
 fn part_01() -> Result<usize> {
     let data = read_input()?;
     let counts: Vec<Counts> = data.iter().map(|d| count(d.as_str())).collect();
@@ -76,7 +117,23 @@ fn part_01() -> Result<usize> {
     Ok(double_count * triple_count)
 }
 
+/// Solution for part 2 -> derive correct ID
+fn part_02() -> Option<String> {
+    let data = read_input().ok()?;
+    match find_candidates(data) {
+        Some((w1, w2)) => {
+            println!("[Part 2] Candidate pair: {} <--> {}", w1, w2);
+            let solution: String = w1.chars().zip(w2.chars())
+                .filter_map(|(c1, c2)| if c1 == c2 { Some(c1) } else { None })
+                .collect();
+            Some(solution)
+        }
+        _ => None
+    }
+}
+
 fn main() -> Result<()> {
     println!("[Part 1] Checksum: {}", part_01()?);
+    println!("[Part 2] Solution ID: {}", part_02().expect("No solution for part 2"));
     Ok(())
 }
